111d88f971efb38b5910d8fad937461c06060542
duyen <duyen.dinh25@student.passerellesnumeriques.org>
2024-05-28 16:52:03 +0700

    100%

--- src/global-components/input/SearchBar.jsx
+++ src/global-components/input/SearchBar.jsx
@@ -1,34 +1,42 @@
-import "./Search.css"
+import "./Search.css";
 import { Input, Space } from 'antd';
 import axios from "axios";
-// import axios from "axios";
+
 const { Search } = Input;
+
 const SearchBar = () => {
   const onSearch = (data) => { 
     searchData(data);
   };
+
   const searchData = async (data) => {
     try {
-      const res = await axios.get("http://127.0.0.1:8000/api/search", {data});
+      const res = await axios.get("http://127.0.0.1:8000/api/search", {
+        params: { query: data }
+      });
       console.log(res);
     } catch (error) {
-      console.log("Search Failed",error);
+      console.log("Search Failed", error);
     }
-  }
-    return (
-      <>
-        <Space direction="vertical">
-            <Search
-                placeholder="input search text"
-                allowClear
-                onSearch={onSearch}
-                style={{
-                  width: 200,
-            }}
-            className="search"
-            />
-        </Space>
-      </>
-  )
-}
+  };
+
+  return (
+    <>
+      <Space direction="vertical">
+        <Search
+          className="custom-search-input"
+          placeholder="Search..."
+          allowClear
+          onSearch={onSearch}
+          style={{
+            width: 570,
+            height: 61,
+            
+          }}
+        />
+      </Space>
+    </>
+  );
+};
+
 export default SearchBar;
