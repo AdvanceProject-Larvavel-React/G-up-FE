12704ac1faacb8942ef2dc5e9e99f1695482f6f6
LanAnh <anh.pham25@student.passerellesnumeriques.org>
2024-05-28 11:43:37 +0700

    FavoriteButton_70%

--- src/modules/DetailProduct/components/FavoriteButton.jsx
+++ src/modules/DetailProduct/components/FavoriteButton.jsx
@@ -1,12 +1,11 @@
 
 import '../styles/FavoriteButton.css';
-import heartIcon from '../../../assets/heart.png';
-
+import heartEmoji from '../../../assets/images/love-03.jpg';
 const FavoriteButton = () => {
   return (
     <button className="favorite-button">
       <div className="favorite-button-heart">
-        <img src={heartIcon} alt="Heart Icon" />
+        <img className="favorite-button-image" src={heartEmoji} alt="Heart Icon" />
       </div>
       <div className="favorite-button-text">Favorite</div>
     </button>
