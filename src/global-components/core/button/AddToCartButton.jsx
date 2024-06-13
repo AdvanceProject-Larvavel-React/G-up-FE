5750b74f84871f1303c65464451603c0ea2df504
LanAnh <anh.pham25@student.passerellesnumeriques.org>
2024-06-13 17:09:45 +0700

    addToCart

--- src/global-components/core/button/AddToCartButton.jsx
+++ src/global-components/core/button/AddToCartButton.jsx
@@ -1,12 +1,30 @@
-
+import  { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { Button } from 'antd';
 
-const AddToCartButton = ({idProd}) => {
-  console.log(idProd);
+const AddToCartButton = ({ idProd }) => {
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const storedUserId = localStorage.getItem('userId');
+    setUserId(storedUserId);
+  }, []);
+
+  const handleAddToCart = () => {
+    if (userId && idProd) {
+      console.log(`Adding product ${idProd} to cart for user ${userId}`);
+    } else {
+      console.error('User ID or Product ID is missing');
+    }
+  };
   return (
-    <Button type="primary" icon={<FontAwesomeIcon icon={faShoppingCart} />} style={{ backgroundColor: '#28a745', borderColor: '#28a745', margin: '0 8px' }}>
+    <Button
+      type="primary"
+      icon={<FontAwesomeIcon icon={faShoppingCart} />}
+      style={{ backgroundColor: '#28a745', borderColor: '#28a745', margin: '0 8px' }}
+      onClick={handleAddToCart}
+    >
       Add to Cart
     </Button>
   );
